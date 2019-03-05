b183b58cb3d21e1f75c9162df46980a39bfad67d
nafiantaBp1996 <nafiantabudi96@gmail.com>
2019-03-05 14:01:51 +0700

    fix data

--- application/views/reguler.php
+++ application/views/reguler.php
@@ -6,15 +6,13 @@
             <div class="col-lg-12 col-md-12">
                 <div style="background:#1b1919; padding:50px;">
                     <h3 class="mb-30 title_color" style="color:#cc9933;" align="center">Kreasi Reguler</h3>
-                
-                    <form action="#">
                         <div class="mt-10">
                             <input class="form-control" type="number" id="pinjaman_input" name="pinjaman" placeholder="pinjaman" value=''>
                         </div>
                         <div class="input-group-icon mt-10">
                             <div class="icon"><i class="fa fa-globe" aria-hidden="true"></i></div>
                             <div class="form-select" id="tenor_id">
-                                <select>
+                                <select  name="tenor" id="tenor_input" class="form-control" required="required" >
                                 <option value="12">12 Bulan</option>
                                 <option value="18">18 Bulan</option>
                                 <option value="24">24 Bulan</option>
@@ -24,12 +22,11 @@
                             </div>
                         </div>
                         <br><br>
-                        <div align="center">
-                            <h1 style="color:#cc9933;">Rp. 120.0000,- /bulan</h1>
+                        <div align="center" id="angsuranShow">
+                            <h1 style="color:#cc9933;"></h1>
                         </div>
                         <br>
-                        <button type="submit" style="background-color:#cc9933!important;" class="template-btn col-lg-12">Detail</button>
-                    </form>
+                        <button type="submit" onclick="getAngsuran()" style="background-color:#cc9933!important;" class="template-btn col-lg-12">Detail</button>
                 </div>
                <br>
             </div>
@@ -37,3 +34,20 @@
       </div>
    </div>
 </div>
+<script>
+    function getAngsuran(){
+        var pinjaman = document.getElementById('pinjaman_input').value;
+        var tenor = document.getElementById('tenor_input').value;
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost/SimluasiPegadaian_API/index.php/reguler',
+            data: {up:pinjaman, tenor:tenor},
+            dataType: 'json',
+            success : function(response){
+                document.getElementById("angsuranShow").innerHTML ="<h1 style='color:#cc9933;''> Rp "+new Intl.NumberFormat().format(response.angsuran)+" / Bulan</h1>"
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+            alert("some error");}
+        });
+    }
+</script>
