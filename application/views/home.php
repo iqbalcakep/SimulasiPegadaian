972c5b5e6c247fbfe578dc8eaf31437bc8b58fd4
nafiantaBp1996 <nafiantabudi96@gmail.com>
2019-03-06 09:15:50 +0700

    fix reguler

--- application/views/home.php
+++ application/views/home.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <center>
             <div class="logo-area">
-                <a href="index.html"><img src="assets/images/logold.png" style="width:40%; height:auto;" alt="logo"></a>
+                <a href="index.html"><img src=" <?php echo base_url('') ?>assets/images/logold.png" style="width:40%; height:auto;" alt="logo"></a>
             </div></center><br><br>
         </div>
     </div>
@@ -24,7 +24,7 @@
                 <div class="col-lg-6 col-md-6">
                     <div class="single-category text-center mb-4">
                         <a href="<?php echo site_url('reguler') ?>">
-                            <img src="assets/images/icon1.png" style="width:128px;height:auto;"alt="category">
+                            <img src="<?php echo base_url('')?>assets/images/icon1.png" style="width:128px;height:auto;"alt="category">
                             <h3>Kreasi Reguler</h3>
                         </a>
                     </div>
@@ -32,7 +32,7 @@
                 <div class="col-lg-6 col-md-6">
                     <div class="single-category text-center mb-4">
                         <a href="<?php echo site_url('reguler') ?>">
-                            <img src="assets/images/icon2.png" style="width:128px;height:auto;"alt="category">
+                            <img src="<?php echo base_url('') ?>assets/images/icon2.png" style="width:128px;height:auto;"alt="category">
                             <h3>Kreasi Fleksi</h3>
                         </a>
                     </div>
@@ -40,7 +40,7 @@
                 <div class="col-lg-6 col-md-6">
                     <div class="single-category text-center mb-4">
                         <a href="<?php echo site_url('reguler') ?>">
-                            <img src="assets/images/icon3.png" style="width:128px;height:auto;"alt="category">
+                            <img src="<?php echo base_url('') ?>assets/images/icon3.png" style="width:128px;height:auto;"alt="category">
                             <h3>Kreasi Fleksi Berjangka</h3>
                         </a>
                     </div>
@@ -48,7 +48,7 @@
                 <div class="col-lg-6 col-md-6">
                     <div class="single-category text-center mb-4">
                         <a href="<?php echo site_url('reguler') ?>">
-                            <img src="assets/images/icon4.png" style="width:128px;height:auto;"alt="category">
+                            <img src="<?php echo base_url('') ?>assets/images/icon4.png" style="width:128px;height:auto;"alt="category">
                             <h3>Kreasi Multiguna</h3>
                         </a>
                     </div>
