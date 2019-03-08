1c96a7706a510b608d21473d7ea5f091f804143a
nafiantaBp1996 <nafiantabudi96@gmail.com>
2019-03-08 08:46:22 +0700

    new multiguna

--- application/views/multiguna.php
+++ application/views/multiguna.php
@@ -1,18 +1,19 @@
 
    <!-- Pricing Table Starts -->
-   <section class="pricing-table section-padding">
+    <section class="pricing-table section-padding">
             <div class="row">
                 <div class="col-md-12">
                     <div class="single-table text-center mb-4 mb-md-0">
                         <div class="table-top">
-                            <h3>Kreasi Fleksi Sekali Bayar</h3>
+                            <h3>Kreasi Multiguna</h3>
                             <i class="fa fa-money"></i>
                         </div>
                         <ul class="my-5" style="padding:30px;">
-                            <?php echo form_open('SekaliBayar/detail'); ?>
+                            <?php echo form_open('multigunagit /detail'); ?>
                             <li class="mb-2">
                                 <div class="mt-10">
-                                    <input type="text"  oninput="getTenor()" placeholder="Jumlah Uang" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Accent color'" required class="single-input-accent"  id="pinjaman_input" name="uang_pinjaman"  data-type="currency">
+
+                                    <input type="text" oninput="getTenor()" placeholder="Jumlah Uang" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Accent color'" class="single-input-accent"  id="pinjaman_input" name="uang_pinjaman"  data-type="currency" required >
                                 </div>
                             </li><br>
                             <li class="mb-2">
@@ -29,8 +30,8 @@
                         <div align="center" id="angsuranShow">
                         </div>
                         <br><br>
-                        <a onclick="getAngsuran()" class="template-btn">Hitung Pelunasan</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button type="submit" class="template-btn">Detail Pinjaman</button>
+                        <a onclick="getAngsuran()" class="template-btn">Hitung Angsuran</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <button type="submit"  class="template-btn">Detail Pinjaman</button>
                         <?php echo form_close(); ?>
                     </div>
                 </div>
@@ -44,8 +45,14 @@
         var pinjaman = document.getElementById('pinjaman_input').value;
         var pin = parseInt(pinjaman.replace(/,/g,""));
         var html = "";
-       
-            var html = "<div class='icon'></div><div class='form-select' id='tenor_id'><select name='tenor_pinjaman' id='tenor_input' class='form-control' required='required' ><option value='3'>03 Bulan</option><option value='4'>04 Bulan</option><option value='6'>06 Bulan</option></select>";
+        if (pin<200000001)
+        {
+            var html = "<div class='icon'></div><div class='form-select' id='tenor_id'><select name='tenor_pinjaman' id='tenor_input' class='form-control' required='required' ><option value='12'>12 Bulan</option><option value='18'>18 Bulan</option><option value='24'>24 Bulan</option><option value='36'>36 Bulan</option></select>";
+        }
+        else
+        {
+            alert("Maksimal Rp 200.000.000")
+        }
          document.getElementById("tenors").innerHTML = html; 
     }
     function getAngsuran(){
@@ -54,11 +61,11 @@
         var pin = pinjaman.replace(/,/g,"");
         $.ajax({
             type: "POST",
-            url: 'http://localhost/SimluasiPegadaian_API/index.php/FleksiSekaliBayar',
+            url: 'http://localhost/SimluasiPegadaian_API/index.php/multiguna',
             data: {up:pin, tenor:tenor},
             dataType: 'json',
             success : function(response){
-                document.getElementById("angsuranShow").innerHTML ="<h2> Rp "+new Intl.NumberFormat().format(response.angsuran)+"</h1>"
+                document.getElementById("angsuranShow").innerHTML ="<h2> Rp "+new Intl.NumberFormat().format(response.angsuran)+" / Bulan</h1>"
             },
             error: function(XMLHttpRequest, textStatus, errorThrown) {
             alert("some error");}
